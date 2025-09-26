7212f735fd54f2d46678718aad21f8eb51347c32
egch <bruhmail>
2025-09-26 19:40:44 +0300

    ind

--- lab2/prog2.c
+++ lab2/prog2.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-void main() {
+#define BUFF_SIZE 4096;
 
+int main() {
+	char dir1[256], dir2[256];
+	int procNum;
+
+	printf("Введите 1 путь к папке: ");
+	scanf("%256s", dir1);
+	printf("Введите 2 путь к папке: ");
+	scanf("%256s", dir2);
+	printf("Введите количество процессов: ");
+	scanf("%d", &procNum);
+
+	DIR *d1 = opendir(dir1);
+	DIR *d2 = opendir(dir2);
+
+	if(!d1 || !d2){
+		perror("Can't open dirs");
+		return 1;
+	}
+
+	closedir(d1);
+	closedir(d2);
+	return 0;
 }
