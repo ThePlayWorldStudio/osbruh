9023e9982587ca9ba7fbb28f6c8d6336ba189783
egch <bruhmail>
2025-11-21 22:30:56 +0300

    lab4

--- lab4/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brew/gti/learn/sem3/os/labother/lab4
-# Build directory: /home/brew/gti/learn/sem3/os/labother/lab4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestAll "/home/brew/gti/learn/sem3/os/labother/lab4/build/tests")
-set_tests_properties(TestAll PROPERTIES  _BACKTRACE_TRIPLES "/home/brew/gti/learn/sem3/os/labother/lab4/CMakeLists.txt;10;add_test;/home/brew/gti/learn/sem3/os/labother/lab4/CMakeLists.txt;0;")
